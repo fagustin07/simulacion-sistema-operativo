8ae1442f3332ea73898b54ab20591d5bc3a03015
Federico Sandoval <fede.sandoval99@gmail.com>
2020-09-06 22:22:46 -0300

    Enunciado + codigo de practica 1 y 2

---
+++ practica_2/main.py
@@ -0,0 +1,28 @@
+from hardware import HARDWARE, ASM
+import log
+from so import Kernel, Program
+##
+##  MAIN
+##
+
+if __name__ == '__main__':
+    log.setupLogger()
+    log.logger.info('Starting emulator')
+
+    ## setup our hardware and set memory size to 20 "cells"
+    HARDWARE.setup(20)
+
+    ## Switch on computer
+    HARDWARE.switchOn()
+
+    ## new create the Operative System Kernel
+    # "booteamos" el sistema operativo
+    kernel = Kernel()
+
+    ##  create a program
+    prg = Program("test.exe", [ASM.CPU(2), ASM.IO(), ASM.CPU(3), ASM.IO(), ASM.CPU(3)])
+    
+    # execute the program
+    kernel.run(prg)
+
+
